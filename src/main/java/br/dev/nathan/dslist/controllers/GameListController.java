5ab364e5ef7697af8a2c2d25c158b6346c9695e2
unknown <nathansilvacosta2303@gmail.com>
2025-08-25 21:54:59 -0300

    Adicionado configuração de CORS nos Controllers para permitir a conexão do Frontend

--- src/main/java/br/dev/nathan/dslist/controllers/GameListController.java
+++ src/main/java/br/dev/nathan/dslist/controllers/GameListController.java
@@ -3,6 +3,7 @@ package br.dev.nathan.dslist.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +19,7 @@ import br.dev.nathan.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "lists")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class GameListController {
 	
 	@Autowired
